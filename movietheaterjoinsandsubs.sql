231d36107c529e13f63321f24a62a0880d790910
Felix Lopez <81645633+frlopezusc69@users.noreply.github.com>
2021-05-06 07:30:20 -0700

    Add files via upload

--- movietheaterjoinsandsubs.sql
+++ movietheaterjoinsandsubs.sql
@@ -29,7 +29,21 @@ Group by store_id, first_name, last_name;
 
 -- Question 3 Show all customer names who have made payments over $175 (use subqueries)
 
+SELECT customer_id
+FROM payment 
+Group by customer_id
+Having SUM(amount) > $175
 
+SELECT first_name, last_name
+From customer
+WHERE customer_id IN (
+    SELECT customer_id
+    FROM payment 
+    Group by customer_id
+    Having SUM(amount) >= $175
+)
+
+Group by store_id, first_name, last_name;
 
 -- Question 4 List all customers that live in Nepal
 
@@ -45,8 +59,57 @@ WHERE country = 'Nepal';
 
 -- Question 5 Which staff member had the most transactions?
 
+SELECT staff_id
+FROM payment 
+Group by staff_id
+Having Count(*) > 2
+
+SELECT first_name, last_name
+From staff
+WHERE staff_id IN (
+    SELECT staff_id
+    FROM payment 
+    Group by staff_id
+    Having Count(*) > 2
+)
+
+Group by staff_id, first_name, last_name;
+
 -- Question 6 What film had the most actors in it?
+
+Select first_name, last_name, Count(*)
+From actor
+Join film_actor
+On actor.actor_id = film_actor.actor_id
+Where film_actor.actor_id IN (
+    Select film_id
+    From film
+    where Count(*) > 2 ASC
+)
+Group by actor.actor_id, first_name, last_name
+Order by actor.actor_id;
 
 -- Question 7 Which actor has been in the least movies?
 
+Select first_name, last_name, Count(*)
+From actor
+Join film_actor
+On actor.actor_id = film_actor.actor_id
+Where film_actor.actor_id IN (
+    Select film_id
+    From film
+    where Count(*) < 2 ASC
+)
+Group by actor.actor_id, first_name, last_name
+Order by actor.actor_id;
+
 -- Question 8 How many districts have more than 5 customers in it?
+
+Select customer.first_name, customer.last_name, customer.email,
+From customer
+FULL JOIN address
+ON customer.address_id = address.address_id
+FULL JOIN city
+ON address.city_id = city.city_id
+Select Count(*) from customer group by district
+Where count(*) > 5
